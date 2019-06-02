e37f7badf57ff8484ab8b40dfeac2566e0085b8d
Dailton Bastos <dailtonbastos@gmail.com>
2019-06-01 23:40:53 -0300

    Metaprogramação para o login Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
     def layout_by_resource
-      devise_controller? && resource_class == Admin ? "admin_devise" : "application"
+      # Metaprogramação
+      devise_controller? ? "#{resource_class.to_s.downcase }_devise" : "application"
     end
 end
